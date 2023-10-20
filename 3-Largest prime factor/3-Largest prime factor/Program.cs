bd664de2358aee8c6e49778171b61ef4c1abb7e8
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-10-20 12:54:13 +0300

    Add files via upload

---
+++ 3-Largest prime factor/3-Largest prime factor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _3_Largest_prime_factor
+{
+    internal class Program
+    {
+        bool IsPrime(long n) // reference: https://en.wikipedia.org/wiki/Primality_test
+        {
+            if (n <= 1)
+                return false;
+
+            if (n == 2 || n == 3)
+                return true;
+
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (long i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            long number = 600851475143;
+            long biggestFactor = 0;
+
+            for (long i = 2; i <= number; i++)
+            {
+                if (number % i == 0 && pr.IsPrime(i))
+                {
+                    biggestFactor = i;
+                    number /= i;
+                }
+            }
+
+            Console.WriteLine("Biggest prime factor of 600851475143 is: " + biggestFactor.ToString());
+        }
+    }
+}
