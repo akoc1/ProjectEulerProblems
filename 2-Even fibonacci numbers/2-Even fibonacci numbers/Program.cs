0ea559d83120a8723c977ba59fbf1ba4718d478f
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-10-20 12:47:50 +0300

    Update Program.cs

--- 2-Even fibonacci numbers/2-Even fibonacci numbers/Program.cs
+++ 2-Even fibonacci numbers/2-Even fibonacci numbers/Program.cs
@@ -7,26 +7,24 @@ namespace _2_Even_fibonacci_numbers
         int FindFibonacci()
         {
             int limit = 4000000, a = 1, b = 1, c = 0, sum = 0;
-
+        
             while (c < limit)
             {
                 c = a + b;
                 a = b;
                 b = c;
-
+        
                 if (c % 2 == 0)
                     sum += c;
             }
-
-            Console.WriteLine("The sum of Fibonacci numbers less than 4 million divisible by 2: " + sum.ToString());
             
-            return 0;
+            return sum;
         }
         
         public static void Main(string[] args)
         {
             Program pr = new Program();
-            pr.FindFibonacci();
+            Console.WriteLine(pr.FindFibonacci());
         }
     }
 }
