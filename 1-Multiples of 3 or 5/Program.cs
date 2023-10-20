b9e55fa8561e8995af9bb7ac9a0e2d2fff05bb69
akoc1 <72667213+akoc1@users.noreply.github.com>
2023-10-20 12:42:05 +0300

    Add files via upload

---
+++ 1-Multiples of 3 or 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1_Multiples_of_3_or_5
+{
+    internal class Program
+    {
+        int FindMultiples()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 5 == 0 || i % 3 == 0)
+                    sum = sum + i;
+            }
+
+            return sum;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Program pr = new Program();
+            Console.WriteLine(pr.FindMultiples());
+        }
+    }
+}
